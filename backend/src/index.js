d03885e344aab9f44001048b4910cec62bdf58b4
Pavel Popov <pcveykos@gmail.com>
2022-12-19 20:36:29 +0100

    Add frontend login functionality, install cookie-parser to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const cors = require('cors')
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const app = require('./app');
 const authRouter = require('./authRoutes/authRouter')
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 app.use('/auth', authRouter)
 app.listen(PORT, (err) => {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-
+import {useState} from 'react'
+import axios from 'axios'
 function Login() {
+
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
+
+
   function handleClick(e) {
     e.preventDefault()
-    console.log('Clicked')
+    const reqBody = {username, password}
+    axios.post('http://localhost:8000/auth/login', reqBody)
+    .then(res => {
+      
+    })
+    .catch(err => console.log(err))
   }
+
   return (
     <div>
     Hello from login page
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" name="username" id="username" onChange={e => setUsername(e.target.value)} />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" onChange={e => setPassword(e.target.value)} />
         <button onClick={e => handleClick(e)}>Log in</button>
       </form>
     </div>
